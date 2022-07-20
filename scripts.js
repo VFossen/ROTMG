3cfa1d5a4010b986f34f264d1a3c99bf95d2f643
Vetle Fossen <103958512+VFossen@users.noreply.github.com>
2022-07-21 00:00:37 +0200

    added "localStorage" to the Event whites.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,22 @@
 const items = [
     'IMG_oreo',
     'IMG_jugg',
-    'IMG_ogmur'
+    'IMG_ogmur',
+    'IMG_kageboshi',
+    'IMG_bloody_cloak',
+    'IMG_keychain_sword',
+    'IMG_blue_bee_quiver',
+    'IMG_red_bee_quiver',
+    'IMG_yellow_bee_quiver',
+    'IMG_qot',
+    'IMG_spirit_trap',
+    'IMG_greaterhosen',
+    'IMG_cdirk',
+    'IMG_ray_katana',
+    'IMG_pogmur',
+    'IMG_tablet',
+    'IMG_conflict',
+    'IMG_stein_poison',
   ];
 
 //Load state of an item
